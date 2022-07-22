6b3c23b0551ba09b06b6579a27cc9512da729a8b
John Doe <johndoe@example.com>
2022-07-22 14:14:50 -0400

    fix bash script

--- example_run_summarization.bash
+++ example_run_summarization.bash
@@ -10,7 +10,6 @@ MAX_OUT_LEN=128
 
 if ! [ -e .already_downloaded_model ]
 then
-    python3 -c "import transformers; transformers.pipeline('summarization', '$MODEL')"
-    touch .already_downloaded_model
+    python3 example_run_summarization.py  --model_name_or_path "$MODEL" --do_train --output_dir output --per_device_train_batch_size="$BATCH_SIZE" --overwrite_output_dir --predict_with_generate --train_file test.json --train_adapter True --num_train_epochs 1 --max_train_samples 1 --max_source_length "$MAX_IN_LEN" --max_target_length "$MAX_OUT_LEN" && touch .already_downloaded_model
 fi
-TRANSFORMERS_OFFLINE=1 python3 example_run_summarization.py  --model_name_or_path "$model" --do_train --output_dir output --per_device_train_batch_size="$BATCH_SIZE" --overwrite_output_dir --predict_with_generate --train_file test.json --train_adapter True --num_train_epochs "$EPOCHS" --max_source_length "$MAX_IN_LEN" --max_target_length "$MAX_OUT_LEN"
+TRANSFORMERS_OFFLINE=1 python3 example_run_summarization.py  --model_name_or_path "$MODEL" --do_train --output_dir output --per_device_train_batch_size="$BATCH_SIZE" --overwrite_output_dir --predict_with_generate --train_file test.json --train_adapter True --num_train_epochs "$EPOCHS" --max_source_length "$MAX_IN_LEN" --max_target_length "$MAX_OUT_LEN"
