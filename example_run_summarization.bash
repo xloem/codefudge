5dd07a3a9fab95778e7f238202befd57ae933a8c
Karl Semich <0xloem@gmail.com>
2022-07-23 12:29:51 +0000

    output dir fixes

--- example_run_summarization.bash
+++ example_run_summarization.bash
@@ -9,11 +9,13 @@ GRAD_ACCUM=2
 EPOCHS=1
 MAX_OUT_LEN=2048
 DATAFILE=test.json
-OUTPUT_DIR="${MODEL##*/}"
+OUTPUT_DIR=fudge-"${MODEL##*/}"
 
-if ! [ -e .already_downloaded_model ]
+mkdir -p "$OUTPUT_DIR"
+if ! [ -e "$OUTPUT_DIR"/.already_downloaded_model ]
 then
-    python3 example_run_summarization.py  --model_name_or_path "$MODEL" --do_train --output_dir "$OUTPUT_DIR" --per_device_train_batch_size="$BATCH_SIZE" --overwrite_output_dir --predict_with_generate --train_file "$DATAFILE" --train_adapter True --num_train_epochs 1 --max_train_samples 1 --max_target_length "$MAX_OUT_LEN" && touch .already_downloaded_model
+    python3 example_run_summarization.py  --model_name_or_path "$MODEL" --do_train --output_dir "$OUTPUT_DIR/tmp" --per_device_train_batch_size="$BATCH_SIZE" --overwrite_output_dir --predict_with_generate --train_file "$DATAFILE" --train_adapter True --num_train_epochs 1 --max_train_samples 1 --max_target_length "$MAX_OUT_LEN" && touch "$OUTPUT_DIR"/.already_downloaded_model
+    rm -rf "$OUTPUT_DIR"/tmp
 fi
 if [ -e "$OUTPUT_DIR"/summarization/adapter_config.json ]
 then
