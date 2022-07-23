56ff834ecafce3936d9737a79d65a24bd3b70d1b
Karl Semich <0xloem@gmail.com>
2022-07-23 15:52:00 +0000

    # bafybeiedn7wjjsnclhgbikfmztq42l5t4fwkhtwdypf7yvjmiidll6zxu4

--- uploading.bash
+++ uploading.bash
@@ -1,11 +1,17 @@
-npm install --global @web3-storage/w3
+w3 --version || {
+	npm install --global @web3-storage/w3
+	npm token
+}
 model=fudge-long-t5-tglobal-base
 while true
 do
 	latest_checkpoint="$(ls -art "$model"/ | grep checkpoint | tail -n 1)"
-	w3 put "$model"/"$latest_checkpoint" | tee w3put.log
+	cp -va "$model"/"$latest_checkpoint" "$model"/"$model"
+	rm "$model"/"$model"/*/pytorch_model_head.bin
+	w3 put "$model"/"$model" | tee w3put.log
 	git add w3put.log
 	git commit -m "$(head -n 1 w3put.log)"
 	git push
+	rm -rf "$model"/"$model"
 	sleep $((60*15))
 done
