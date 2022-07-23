215bc84dbc518b1b9cd7e290aa27e45398b2c87b
John Doe / Karl Semich <0xloem@gmail.com>
2022-07-23 21:28:13 +0000

    pull before push

--- uploading.bash
+++ uploading.bash
@@ -9,6 +9,7 @@ do
 	cp -va "$model"/"$latest_checkpoint" "$model"/"$model"
 	rm "$model"/"$model"/*/pytorch_model_head.bin
 	w3 put "$model"/"$model" | tee w3put.log
+	git pull --no-edit
 	git add w3put.log
 	git commit -m "$(head -n 1 w3put.log)"
 	git push
