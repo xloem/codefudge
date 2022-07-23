d24d9a5c143b778b7bb5f0092931b6ca3a036bf3
Karl Semich <0xloem@gmail.com>
2022-07-23 10:30:31 +0000

    handle missing files

--- hist2json.py
+++ hist2json.py
@@ -11,8 +11,11 @@ with open("test.json", "wt") as output:
     pairs = ((filename, filename[:-len('.file')] + '.commit') for filename in glob.glob('../*.file'))
     for file, commit in pairs:
         assert file[:-len('file')] == commit[:-len('commit')]
-        input = str(charset_normalizer.from_path(file).best())
-        label = str(charset_normalizer.from_path(commit).best())
+        try:
+            input = str(charset_normalizer.from_path(file).best())
+            label = str(charset_normalizer.from_path(commit).best())
+        except:
+            continue
         if len(input) + len(label) < MAX_COMBINED and len(input) < MAX_INPUT and len(label) < MAX_LABEL and len(input) > 0 and len(label) > 0:
             print(file, len(input), commit, len(label))
             output.write(json.dumps({
