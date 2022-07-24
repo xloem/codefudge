adcb490bb1508653d0a5592b9bf274e8f260ccb5
xloem <0xloem@gmail.com>
2022-07-24 05:48:32 -0400

    web update: ipfs download fix

--- run.bash
+++ run.bash
@@ -46,10 +46,10 @@ done &
 
 # download recent model to work off of
 read hash cid < w3put.log
-curl --head https://dweb.link/ipfs/"$b" | tr -d '\r' | grep -i ^location: | {
+curl --head https://dweb.link/ipfs/"$cid" | tr -d '\r' | grep -i ^location: | {
 	read location uri
 	wget --mirror "$uri"
-	cp -va *.ipfs.dweb.link/fudge-* 
+	cp -va *.ipfs.dweb.link/fudge-* .
 }
 
 # start grooming
