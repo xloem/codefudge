7c9f280011830d59fe70dc7431fff452976b912c
John Doe <johndoe@example.com>
2022-08-21 05:32:03 -0400

    simplify download to specify gateway

--- download.bash
+++ download.bash
@@ -1,7 +1,8 @@
 # download recent model to work off of
 read hash cid < w3put.log
-curl --head https://dweb.link/ipfs/"$cid" | tr -d '\r' | grep -i ^location: | {
-	read location uri
+uri=https://"${cid}".ipfs.dweb.link/
+#curl --head https://dweb.link/ipfs/"$cid" | tr -d '\r' | grep -i ^location: | {
+#	read location uri
 	wget --mirror "$uri"
     cp -va "${uri#*//}"fudge-* .
-}
+#}
