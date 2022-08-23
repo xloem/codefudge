0ab416d2b170dc2b8a1c60dc1b8ad370762b5a51
uploadscript <upload@example.com>
2022-08-23 09:11:18 +0000

    updated from within notebook [tried to add direct ipfs download]

--- download.bash
+++ download.bash
@@ -1,8 +1,16 @@
 # download recent model to work off of
 read hash cid < w3put.log
-uri=https://"${cid}".ipfs.dweb.link/
-#curl --head https://dweb.link/ipfs/"$cid" | tr -d '\r' | grep -i ^location: | {
-#	read location uri
+if ipfs --version
+then
+  ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/9001
+  ipfs daemon &
+  daemonpid=$!
+  sleep 15
+  ipfs get "$hash"
+  kill $daemonpid
+  cp -va "$hash"/fudge-* .
+else
+  uri=https://"${cid}".ipfs.dweb.link/
 	wget --mirror "$uri"
-    cp -va "${uri#*//}"fudge-* .
-#}
+  cp -va "${uri#*//}"fudge-* .
+fi
