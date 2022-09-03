c10b1f3b42de1ae4b2076231bbf471ff4c3246d0
uploadscript <upload@example.com>
2022-09-03 12:31:09 +0000

    gh cli fix

--- run.bash
+++ run.bash
@@ -1,9 +1,14 @@
 # install gh (which makes github push work) and w3, for checkpoint uploading
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+#curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+#chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+#echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+#apt update
+#apt install gh
+
+# gh's key expired and they're having trouble updating it 2022-09-02
+curl https://api.github.com/repos/cli/cli/releases/latest | grep "linux_amd64\\.deb" | cut -d : -f 2,3 | tr -d \" | wget -i -
 apt update
-apt install gh
+apt-get install ./*.deb
 
 npm install --global @web3-storage/w3
 
