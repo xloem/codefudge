0a84a55c64e92fdd78f0df7c77000c6226d40f5d
John Doe <john.doe@example.com>
2022-09-15 08:33:56 -0400

    hyperparams.py

--- hyperparams.py
+++ hyperparams.py
@@ -1,4 +1,5 @@
 import git, json, logging, os, requests, sys
+import math, datetime
 import tqdm
 from collections import defaultdict
 #logging.basicConfig(level=logging.DEBUG)
@@ -8,6 +9,8 @@ repo = git.Repo('.')
 GATEWAY='dweb.link'
 PATH='w3put.log'
 SUBURL='fudge-long-t5-tglobal-base/trainer_state.json'
+DATALEN = 51712
+EARLIEST = datetime.datetime.now().timestamp() - 60 * 60 * 24 * 30
 
 def tree_lookup(tree, name):
     for subtree in tree.trees:
@@ -97,6 +100,8 @@ for url, (blob, commit) in zip(tqdm.tqdm(urls, desc='train states'), blobs.value
             continue
         state = cache[url]
         #epoch_portion_per_step = state['num_train_epochs'] / state['max_steps']
+        #if int(state['max_steps'] / DATALEN) * DATALEN != state['max_steps']:
+        #    continue
         steps_per_epoch = state['max_steps'] / state['num_train_epochs']
         log_history = state['log_history']
         if 'total_flos' in log_history[-1]:
@@ -110,13 +115,36 @@ for url, (blob, commit) in zip(tqdm.tqdm(urls, desc='train states'), blobs.value
                 continue
             seen_logs.add(id)
             lr = (prev['learning_rate'] + next['learning_rate']) / 2
+            loss = (prev['loss'] + next['loss']) / 2
+            step = (prev['step'] + next['step']) / 2
             loss_change_per_epoch = (prev['loss'] - next['loss']) * (next['step'] - prev['step']) / steps_per_epoch
-            entry = (steps_per_epoch, lr, loss_change_per_epoch, commit.authored_date, next['step'])
+            entry = (steps_per_epoch, lr, loss, loss_change_per_epoch, commit.authored_date, step)
             data.append(entry)
     #except:
     #    continue
-for steps_per_epoch, lr, loss_change_per_epoch, authored_date, step in data:
+#def sort_key(elem):
+#    steps_per_epoch, lr, loss, loss_change_per_epoch, authored_date, step = elem
+#
+#data.sort(key = l
+buckets = defaultdict(list)
+for steps_per_epoch, lr, loss, loss_change_per_epoch, authored_date, step in data:
+    if authored_date < EARLIEST:
+        continue
     print(steps_per_epoch, lr, loss_change_per_epoch, authored_date)
+    lr_exp = 10 ** int(math.log(lr) / math.log(10))
+    rounded_lr = round(lr / lr_exp * 20) * lr_exp / 20
+    #rounded_steps_per_epoch = round(steps_per_epoch / 1000) * 1000
+    grad_accum = round(DATALEN / steps_per_epoch)
+    buckets[(rounded_lr, grad_accum)].append((loss, loss_change_per_epoch, authored_date))
+options = [(rounded_lr, grad_accum, elems) for (rounded_lr, grad_accum), elems in buckets.items()]
+print('\n')
+print('lr', 'grad_accum', '# items', 'loss_change_per_epoch')
+options.sort(key = lambda option: sum((loss_change_per_epoch for loss, loss_change_per_epoch, authored_date in option[2]))/len(option[2]))
+for rounded_lr, grad_accum, elems in options:#[-16:]:
+    print(rounded_lr, grad_accum, f'{len(elems)}x',
+            sum((loss_change_per_epoch for loss, loss_change_per_epoch, authored_date in elems))/len(elems),
+            datetime.datetime.fromtimestamp(sum((authored_date for loss, loss_change_per_epoch, authored_date in elems))/len(elems)).isoformat()
+    )
 #example cache content:
 # {
 #     "https://gateway.pinata.cloud/ipfs/bafybeigr7wwnxzkxd4sg542fhhwxo4vqvo5wwsj2q4w6tfvbp2rckurc3y": {
