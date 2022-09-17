6171f91bcb7ad3dc41c4784b98d481b74aa21f7a
John Doe <john.doe@example.com>
2022-09-17 04:15:26 -0400

    add a fix for <1.5 hyperp data and debugging output

--- hyperparams.py
+++ hyperparams.py
@@ -1,5 +1,5 @@
 import git, json, logging, os, requests, sys
-import math, datetime
+import math, datetime, shutil
 import tqdm
 from collections import defaultdict
 #logging.basicConfig(level=logging.DEBUG)
@@ -12,6 +12,7 @@ SUBURL='fudge-long-t5-tglobal-base/trainer_state.json'
 DATALEN = 51712
 EARLIEST = datetime.datetime.now().timestamp() - 60 * 60 * 24 * 28
 GRAD_ACCUM_MAX = None #4
+SUBDIVISIONS = 1
 
 def tree_lookup(tree, name):
     for subtree in tree.trees:
@@ -127,27 +128,35 @@ for url, (blob, commit) in zip(tqdm.tqdm(urls, desc='train states'), blobs.value
 #    steps_per_epoch, lr, loss, loss_change_per_epoch, authored_date, step = elem
 #
 #data.sort(key = l
+try:
+    shutil.rmtree('hyperp-dbg')
+except:
+    pass
+os.mkdir('hyperp-dbg')
 buckets = defaultdict(list)
-for steps_per_epoch, lr, loss, loss_change_per_epoch, authored_date, step in data:
-    print(steps_per_epoch, lr, loss_change_per_epoch, authored_date)
+for idx, (steps_per_epoch, lr, loss, loss_change_per_epoch, authored_date, step) in enumerate(data):
+    print(idx, steps_per_epoch, lr, loss_change_per_epoch, authored_date)
     grad_accum = round(DATALEN / steps_per_epoch)
     scaled_lr = lr / grad_accum
-    lr_exp = 10 ** int(math.log(scaled_lr) / math.log(10) - 0.5)
-    rounded_lr = round(scaled_lr / lr_exp * 4) * lr_exp / 4
+    lr_exp = 10 ** int(math.log(scaled_lr) / math.log(10) - 1.5)
+    rounded_lr = round(scaled_lr / lr_exp * SUBDIVISIONS) * lr_exp / SUBDIVISIONS
     #rounded_steps_per_epoch = round(steps_per_epoch / 1000) * 1000
     if authored_date < EARLIEST:
         continue
     if GRAD_ACCUM_MAX is not None and grad_accum > GRAD_ACCUM_MAX:
         continue
-    buckets[(rounded_lr, grad_accum)].append((loss, loss_change_per_epoch, authored_date))
+    with open(f'hyperp-dbg/{rounded_lr}-{grad_accum}', 'at') as f:
+        f.write(f'{idx} {lr} {loss} {loss_change_per_epoch} {authored_date}\n')
+    buckets[(rounded_lr, grad_accum)].append((idx, loss, loss_change_per_epoch, authored_date))
 options = [(rounded_lr, grad_accum, elems) for (rounded_lr, grad_accum), elems in buckets.items()]
 print('\n')
 print('lr', 'grad_accum', '# items', 'loss_change_per_epoch')
-options.sort(key = lambda option: sum((loss_change_per_epoch for loss, loss_change_per_epoch, authored_date in option[2]))/len(option[2]))
+options.sort(key = lambda option: sum((loss_change_per_epoch for idx, loss, loss_change_per_epoch, authored_date in option[2]))/len(option[2]))
 for rounded_lr, grad_accum, elems in options:#[-16:]:
     print(rounded_lr, grad_accum, f'{len(elems)}x',
-            sum((loss_change_per_epoch for loss, loss_change_per_epoch, authored_date in elems))/len(elems),
-            datetime.datetime.fromtimestamp(sum((authored_date for loss, loss_change_per_epoch, authored_date in elems))/len(elems)).isoformat()
+            sum((loss_change_per_epoch for idx, loss, loss_change_per_epoch, authored_date in elems))/len(elems),
+            datetime.datetime.fromtimestamp(sum((authored_date for idx, loss, loss_change_per_epoch, authored_date in elems))/len(elems)).isoformat(),
+            #*(idx for idx, loss, loss_change_per_epoch, authored_date in elems)
     )
 #example cache content:
 # {
